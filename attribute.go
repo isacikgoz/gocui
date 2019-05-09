d503aca0bf8d0e950b1846e99cef312a81ba77c2
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-05-09 14:24:58 +0300

    fix colors

--- attribute.go
+++ attribute.go
@@ -4,29 +4,27 @@
 
 package gocui
 
-import "github.com/nsf/termbox-go"
-
 // Attribute represents a terminal attribute, like color, font style, etc. They
 // can be combined using bitwise OR (|). Note that it is not possible to
 // combine multiple color attributes.
-type Attribute termbox.Attribute
+type Attribute uint16
 
 // Color attributes.
 const (
-	ColorDefault Attribute = Attribute(termbox.ColorDefault)
-	ColorBlack             = Attribute(termbox.ColorBlack)
-	ColorRed               = Attribute(termbox.ColorRed)
-	ColorGreen             = Attribute(termbox.ColorGreen)
-	ColorYellow            = Attribute(termbox.ColorYellow)
-	ColorBlue              = Attribute(termbox.ColorBlue)
-	ColorMagenta           = Attribute(termbox.ColorMagenta)
-	ColorCyan              = Attribute(termbox.ColorCyan)
-	ColorWhite             = Attribute(termbox.ColorWhite)
+	ColorDefault Attribute = iota
+	ColorBlack
+	ColorRed
+	ColorGreen
+	ColorYellow
+	ColorBlue
+	ColorMagenta
+	ColorCyan
+	ColorWhite
 )
 
 // Text style attributes.
 const (
-	AttrBold      Attribute = Attribute(termbox.AttrBold)
-	AttrUnderline           = Attribute(termbox.AttrUnderline)
-	AttrReverse             = Attribute(termbox.AttrReverse)
+	AttrBold Attribute = 1 << (9 + iota)
+	AttrUnderline
+	AttrReverse
 )
